3669cfaee6f24501570e1286f0205f0f00cd3986
Felipe Izaguire <fmizaguirre@inf.ufrgs.br>
2018-09-17 23:58:05 -0300

    code reestructure

---
+++ client/testDynamix.js
@@ -0,0 +1,105 @@
+function ferr(e)
+{
+  console.log(e);
+}
+
+if (Meteor.isClient) {
+
+  Deps.autorun(function(){
+    Meteor.subscribe('offers');
+  });
+
+  Template.hello.greeting = function () {
+    return "Welcome to testDynamix.";
+  };
+
+  Template.hello.events({
+    'click input' : function () {
+      // template data, if any, is available in 'this'
+      if (typeof console !== 'undefined')
+        console.log("You pressed the button");
+    },
+    'click .logout': function(evt) {
+      
+      evt.preventDefault();
+      Meteor.logout(function(error){
+        if(error) {
+          console.log(error.reason);
+        }
+      });
+    }
+  });
+
+  Template.register.events({
+    'submit form': function(evt) {
+      evt.preventDefault();
+
+      var registerUser = {
+        username: evt.target.emailReg.value,
+        email: evt.target.emailReg.value,
+        password: evt.target.passlReg.value
+      }
+
+      Accounts.createUser(registerUser, function(error){
+        if(Meteor.user())
+        {
+          console.log(Meteor.user());
+          Principal.create("as","as_" + Meteor.user().username + "_offers", Principal.user(), ferr)
+        }
+        else
+        {
+          console.log(error.reason);
+        }
+      })
+    }
+  });
+
+  Template.login.events({
+    'submit form': function(evt) {
+      evt.preventDefault();
+
+      Meteor.loginWithPassword({email:evt.target.emailLogin.value}, evt.target.passwordLogin.value,
+        function(error) {
+          if(Meteor.user()){
+            console.log(Meteor.user());
+          }
+          else {
+            console.log(error.reason);
+          }
+        });
+    }
+  });
+
+  Template.registerAnOffer.events({
+    'submit form': function(evt) {
+      evt.preventDefault();
+
+      _aspath = evt.target.aspath.value;
+      _userId = Meteor.user().userId;
+      _createdAt = '';
+
+      //Meteor.call('getServerTime', function(e, r) { _createdAt = r; });
+
+
+      Principal.lookup([new PrincAttr("as", "as_" + Meteor.user().username + "_offers")],
+                        Meteor.user().username,
+                        function(userPrincipal) {
+                          Offers.insert({
+                            userId: _userId,
+                            asprinc: userPrincipal.id,
+                            aspath: _aspath,
+                            createdBy: Meteor.userId(),
+                            createdAt: + new Date()
+                            });
+                        });
+
+      evt.target.aspath.value = '';
+    }
+  });
+
+  Template.offer.offers = function() {
+    //return Offers.find({createdBy:Meteor.userId()});
+    return Offers.find();
+  };
+
+}
---
+++ model.js
@@ -0,0 +1,49 @@
+Offers = new Meteor.Collection('offers');
+
+Offers._encrypted_fields({
+  'aspath' : {princ: 'asprinc', princtype: 'as', auth: ['_id']}
+});
+
+function ferr(e)
+{
+  console.log(e);
+}
+
+
+/* trusted IDP: */
+//var idp_pub = '8a7fe03431b5fc2db3923a2ab6d1a5ddf35cd64aea35e743' +
+//              'ded7655f0dc7e085858eeec06e1c7da58c509d57da56dbe6';
+//idp_init("http://localhost:3010", idp_pub, false);
+
+// use IDP only if active attacker
+//Accounts.config({sendVerificationEmail:active_attacker()});
+
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+
+  Meteor.methods({
+    'getServerTime': function() {
+      return + new Date;
+    }
+  });
+
+  Offers.allow({
+  // anyone can insert a new room
+  insert: function (userId, doc) {
+      return true;
+  },
+  // only owner can change room
+  update: function (userId, doc, fields, modifier) {
+      //return doc.createdByID === userId;
+      return true;
+  },
+  // only owner can remove room
+  remove: function (userId, doc) {
+      //return doc.createdByID === userId;
+      return true;
+  }
+  });
+}
